58bb91f6837a2f1ec5afea8c1f4a501ce1be9964
Kondareddy011 <kondareddy5628@gmail.com>
2026-07-25 11:09:22 +0530

    Reduce vertical feed spacing and bottom margin in receipt printing

--- lib/services/bluetooth_printer_service.dart
+++ lib/services/bluetooth_printer_service.dart
@@ -132,7 +132,7 @@ class BluetoothPrinterService {
       pw.Page(
         pageFormat: paperWidth.copyWith(
           marginTop: 8,
-          marginBottom: 12,
+          marginBottom: 4,
           marginLeft: 8,
           marginRight: 8,
         ),
@@ -305,7 +305,7 @@ class BluetoothPrinterService {
     bytes.addAll('--------------------------------\n'.codeUnits);
 
     // Footer
-    bytes.addAll('${config.footerMessage}\n\n\n'.codeUnits);
+    bytes.addAll('${config.footerMessage}\n'.codeUnits);
 
     // GS V 66 0 - Paper cut command
     bytes.addAll([0x1D, 0x56, 0x42, 0x00]);
